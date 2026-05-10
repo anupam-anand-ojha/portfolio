9783ecc50a9aaaa23806c54a88967bc4a5476741
Anupam Anand <theanupamojha999@gmail.com>
2026-05-10 23:38:23 +0530

    change rotate text UI

--- src/components/textRotate.jsx
+++ src/components/textRotate.jsx
@@ -4,12 +4,12 @@ function textRotate() {
   return (
     <span className="text-rotate text-7xl">
   <span className="justify-items-center">
-    <span>DESIGN</span>
-    <span>DEVELOP</span>
+    <span>FRONTEND</span>
+    <span>BACKEND</span>
+    <span>FULLSTACK</span>
+    <span>API</span>
+    <span>DATABASE</span>
     <span>DEPLOY</span>
-    <span>SCALE</span>
-    <span>MAINTAIN</span>
-    <span>REPEAT</span>
   </span>
 </span>
   )
