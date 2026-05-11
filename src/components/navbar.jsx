cfe4ec0780c2755e4a778b264c813a9958fb8703
Anupam Anand <theanupamojha999@gmail.com>
2026-05-11 23:59:25 +0530

    update ui

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,23 +4,17 @@ function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl font-bold">Anupam.dev</a>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
-
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-
-      <li><a>About</a></li>
-
-      <li><a>Projects</a></li>
-
+      <li><a>Link</a></li>
       <li>
         <details>
-          <summary>More</summary>
+          <summary>Parent</summary>
           <ul className="bg-base-100 rounded-t-none p-2">
-            <li><a>Skills</a></li>
-            <li><a>Contact</a></li>
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
           </ul>
         </details>
       </li>
