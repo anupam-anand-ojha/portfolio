8cb5522ccce11e0e2b9100ba3d8814e23b69cb11
Anupam Anand <theanupamojha999@gmail.com>
2026-05-12 23:38:21 +0530

    update projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -80,6 +80,21 @@ function Projects() {
   <div></div>
   <div></div>
 </div>
+<div className="hover-3d">
+  {/* content */}
+  <figure className="w-60 rounded-2xl">
+    <img src="https://img.daisyui.com/images/stock/card-3.webp?x" alt="Tailwind CSS 3D hover" />
+  </figure>
+  {/* 8 empty divs needed for the 3D effect */}
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
 
 </div>
   )
