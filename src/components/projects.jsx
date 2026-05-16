e0651f0eb08f3466e24ce80aa158fcb89cc6fc64
Anupam Anand <theanupamojha999@gmail.com>
2026-05-16 23:45:39 +0530

    add projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function Projects() {
   return (
+ <div className='min-h-screen'>
  <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
 
   {/* CARD 1 */}
@@ -27,8 +28,8 @@ function Projects() {
 
   {/* CARD 2 */}
   <div className="hover-3d">
-    <figure className="max-w-100 rounded-2xl relative">
-      <img src="https://img.daisyui.com/images/stock/photo-1492724441997-5dc865305da7.webp" alt="3D card" />
+     <figure className="max-w-100 rounded-5xl relative">
+       <img src="https://img.daisyui.com/images/stock/creditcard.webp" alt="3D card" />
 
       <div className="absolute bottom-0 left-0 p-4 text-white bg-black/50 w-full rounded-b-2xl">
         <h2 className="font-bold text-lg">E-Commerce Platform</h2>
@@ -48,8 +49,72 @@ function Projects() {
 
   {/* CARD 3 */}
   <div className="hover-3d">
-    <figure className="max-w-100 rounded-2xl relative">
-      <img src="https://img.daisyui.com/images/stock/photo-1556745757-8d76bdb6984b.webp" alt="3D card" />
+     <figure className="max-w-100 rounded-5xl relative">
+       <img src="https://img.daisyui.com/images/stock/creditcard.webp" alt="3D card" />
+
+
+      <div className="absolute bottom-0 left-0 p-4 text-white bg-black/50 w-full rounded-b-2xl">
+        <h2 className="font-bold text-lg">AI Resume Builder</h2>
+        <p className="text-sm mb-3">
+          Generate professional resumes using AI with PDF export feature.
+        </p>
+        <div className="flex gap-2">
+          <button className="btn btn-xs btn-outline text-white border-white">Code</button>
+          <button className="btn btn-xs btn-primary">Live</button>
+        </div>
+      </div>
+    </figure>
+
+    <div></div><div></div><div></div><div></div>
+    <div></div><div></div><div></div><div></div>
+  </div>
+  {/* CARD 4 */}
+  <div className="hover-3d">
+     <figure className="max-w-100 rounded-5xl relative">
+       <img src="https://img.daisyui.com/images/stock/creditcard.webp" alt="3D card" />
+
+
+      <div className="absolute bottom-0 left-0 p-4 text-white bg-black/50 w-full rounded-b-2xl">
+        <h2 className="font-bold text-lg">AI Resume Builder</h2>
+        <p className="text-sm mb-3">
+          Generate professional resumes using AI with PDF export feature.
+        </p>
+        <div className="flex gap-2">
+          <button className="btn btn-xs btn-outline text-white border-white">Code</button>
+          <button className="btn btn-xs btn-primary">Live</button>
+        </div>
+      </div>
+    </figure>
+
+    <div></div><div></div><div></div><div></div>
+    <div></div><div></div><div></div><div></div>
+  </div>
+  {/* CARD 5 */}
+  <div className="hover-3d">
+     <figure className="max-w-100 rounded-5xl relative">
+       <img src="https://img.daisyui.com/images/stock/creditcard.webp" alt="3D card" />
+
+
+      <div className="absolute bottom-0 left-0 p-4 text-white bg-black/50 w-full rounded-b-2xl">
+        <h2 className="font-bold text-lg">AI Resume Builder</h2>
+        <p className="text-sm mb-3">
+          Generate professional resumes using AI with PDF export feature.
+        </p>
+        <div className="flex gap-2">
+          <button className="btn btn-xs btn-outline text-white border-white">Code</button>
+          <button className="btn btn-xs btn-primary">Live</button>
+        </div>
+      </div>
+    </figure>
+
+    <div></div><div></div><div></div><div></div>
+    <div></div><div></div><div></div><div></div>
+  </div>
+  {/* CARD 6 */}
+  <div className="hover-3d">
+     <figure className="max-w-100 rounded-5xl relative">
+       <img src="https://img.daisyui.com/images/stock/creditcard.webp" alt="3D card" />
+
 
       <div className="absolute bottom-0 left-0 p-4 text-white bg-black/50 w-full rounded-b-2xl">
         <h2 className="font-bold text-lg">AI Resume Builder</h2>
@@ -67,6 +132,7 @@ function Projects() {
     <div></div><div></div><div></div><div></div>
   </div>
 
+</div>
 </div>
   )
 }
