7c28892f53aa173d0271f233677e7bf848f14227
Anupam Anand <theanupamojha999@gmail.com>
2026-07-09 23:52:26 +0530

    update resume

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Resume() {
   return (
-   <div className="skeleton h-32 w-32"> here is your resume</div>
+   <div className="skeleton h-32 w-32"> Here is your resume</div>
   )
 }
 
